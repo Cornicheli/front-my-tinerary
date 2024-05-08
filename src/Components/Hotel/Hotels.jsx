ebbe2352a3df7eaaff5cdab032c94078cbb10ac6
cornicheli <gabrielcornide.pg@gmail.com>
2024-05-08 16:51:18 -0300

    complet card hotel

--- src/Components/Hotel/Hotels.jsx
+++ src/Components/Hotel/Hotels.jsx
@@ -3,12 +3,18 @@ import { useDispatch, useSelector } from "react-redux";
 import hotelActions from "../../redux/actions/hotelActions";
 import "../../Styles/HotelCard.css";
 import { CardHotel } from "../card/CardHotel";
+import { ModalDetailsCard } from "../modal/ModalDetailCard";
 
 export default function Hotels() {
   const hotelList = useSelector((store) => store.hotelsReducers.listsHotels);
+  console.log(hotelList)
   const dispatch = useDispatch();
   const [search, setSearch] = useState("");
   const [order, setOrder] = useState("asc");
+  const [showModal, setShowModal] = useState(false);
+  const [selectedHotelId, setSelectedHotelId] = useState(null);
+  const [hotelTitle, setHotelTitle] = useState(null);
+  const [hotelImage, setHotelImage] = useState(null);
 
   useEffect(() => {
     dispatch(hotelActions.getHotels());
@@ -22,33 +28,33 @@ export default function Hotels() {
     dispatch(hotelActions.getHotelsFilters(filter));
   }, [search, order]);
 
-  function DataFetching() {
-    const getRandomImage = (arr) => {
-      const length = arr.length;
-      const randomIndex = Math.floor(length * Math.random());
-      return arr[randomIndex];
-    }
-    return hotelList.map((e) => {
-      return (
-        <CardHotel
-          img={getRandomImage(e.photo)}
-          imgAlt={"image de hotel"}
-          keyImg={e.keyImg}
-          keyTitle={e.id}
-          title={e.name}
-          titleLink={"See more"}
-          keyLink={e.id}
-          toLink={`/DetailsHotel/${e._id}`}
-        />
-      );
-    });
+  const handleOpenModal = (hotelId, hotelTitle, hotelImage, hotelCapacity) => {
+    setSelectedHotelId(hotelId);
+    setHotelTitle(hotelTitle);
+    setHotelImage(hotelImage);
+    setShowModal(true);
+  };
+  console.log(handleOpenModal)
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setHotelTitle(null);
+    setHotelImage(null);
+    setSelectedHotelId(null);
+  };
+
+  const getRandomImage = (arr) => {
+    const length = arr.length;
+    const randomIndex = Math.floor(length * Math.random());
+    return arr[randomIndex];
   }
+
   return (
-    <div>
+    <>
       <div className="flex flex-row items-center justify-center">
         <babel className="text-lg text-black font-semibold">Sort by</babel>
         <select
-          className="underline underline-offset-2	"
+          className="underline underline-offset-2"
           type="select"
           onChange={(e) => {
             let orderer = e.target.value;
@@ -72,8 +78,29 @@ export default function Hotels() {
         </div>
       </div>
       <div className="grid grid-cols-4 gap-4 place-content-center py-4">
-        <DataFetching />
+        {
+          hotelList.map((e) => (
+            <CardHotel
+              keyCard={e._id}
+              img={getRandomImage(e.photo)}
+              imgAlt={"image de hotel"}
+              keyImg={e.keyImg}
+              keyTitle={e.id}
+              title={e.name}
+              onClick={() => handleOpenModal(e._id, e.name, getRandomImage(e.photo))}
+            />
+          ))
+        }
+        {showModal && selectedHotelId && (
+          <ModalDetailsCard
+            hotelId={selectedHotelId}
+            hotelTitle={hotelTitle}
+            hotelImage={hotelImage}
+            handleCloseModal={handleCloseModal}
+            selectedHotel
+          />
+        )}
       </div>
-    </div>
+    </>
   );
 }
--- src/Components/card/CardHotel.jsx
+++ src/Components/card/CardHotel.jsx
@@ -1,22 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { ModalDetailsCard } from "../modal/ModalDetailCard";
 
 export const CardHotel = ({
+      keyCard,
       img,
       imgAlt,
       keyImg,
       title,
       keyTitle,
-      keyLink,
-      toLink,
-      titleLink,
+      onClick,
 }) => {
+
+      const [setshowModal, setSetshowModal] = useState(false)
+
       return (
-            <div className="relative flex w-[400px] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+            <div className="relative flex w-[400px] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md" key={keyCard}>
                   <figure className="relative mx-4 my-2 h-52 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-gray-500 to-gray-600">
                         <img
-                              key={keyImg}
                               className="rounded-t-lg w-full h-full"
+                              key={keyImg}
                               src={img}
                               alt={imgAlt}
                         />
@@ -28,17 +30,19 @@ export const CardHotel = ({
                               {title}
                         </h5>
                   </div>
-                  <div className="p-4 pt-0">
-                        <Link
-                              data-ripple-light="true"
-                              type="button"
-                              className="select-none w-40 rounded-xl bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                              key={keyLink}
-                              to={toLink}
-                        >
-                              {titleLink}
-                        </Link>
+                  <div className="p-4 pt-0" >
+                        <button
+                              onClick={onClick}
+                              className="select-none w-40 rounded-xl bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                              See more
+                        </button>
                   </div>
+                  {
+                        setshowModal &&
+                        <ModalDetailsCard
+                              handleCloseModal={() => setSetshowModal(false)}
+                        />
+                  }
             </div>
       );
 };
